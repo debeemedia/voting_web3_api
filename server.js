a2ecfdfaa659180505793edd0479c10fa62bdda7
Deborah Okeke (deBee) <72860663+debeemedia@users.noreply.github.com>
2023-11-30 10:52:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,107 +16,88 @@ const port = process.env.PORT || 8000
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.PROJECT_ID}`);
 
 const abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_electionName",
-          "type": "string"
-        },
-        {
-          "internalType": "uint256",
-          "name": "_voteChoice",
-          "type": "uint256"
-        }
-      ],
-      "name": "castVote",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_electionName",
-          "type": "string"
-        },
-        {
-          "internalType": "string[]",
-          "name": "_choiceTexts",
-          "type": "string[]"
-        }
-      ],
-      "name": "createElection",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "voter",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "string",
-          "name": "electionName",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "voteChoice",
-          "type": "uint256"
-        }
-      ],
-      "name": "Voted",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "name": "elections",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "electionName",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_electionName",
-          "type": "string"
-        }
-      ],
-      "name": "getResults",
-      "outputs": [
-        {
-          "internalType": "uint256[]",
-          "name": "",
-          "type": "uint256[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_electionName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_voteChoice",
+				"type": "uint256"
+			}
+		],
+		"name": "castVote",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_electionName",
+				"type": "string"
+			},
+			{
+				"internalType": "string[]",
+				"name": "_choiceTexts",
+				"type": "string[]"
+			}
+		],
+		"name": "createElection",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "voter",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "string",
+				"name": "electionName",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "voteChoice",
+				"type": "uint256"
+			}
+		],
+		"name": "Voted",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_electionName",
+				"type": "string"
+			}
+		],
+		"name": "getResults",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ]
-const contractAddress = '0x028F86545cc4F2135Ce6860Ce46D4E722BdC6026';
+const contractAddress = '0x7D823610BA65733AE2Aa6F504f6B6bA3443f9d40';
 // const contract = new web3.eth.Contract(abi, contractAddress);
 const contract = new ethers.Contract(contractAddress, abi, provider);
 
